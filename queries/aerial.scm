194162dac61ff8956fbd57572f2c523ac2ebad6f
Longtao Zhang <DragonBillow@outlook.com>
2023-06-24 09:32:29 +0800

    feat(aerial): 添加对 aerial 支持
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

---
+++ queries/aerial.scm
@@ -0,0 +1,15 @@
+(title0
+  (#set! "kind" "Interface")
+  ) @name @type
+(title1
+  (#set! "kind" "Interface")
+  ) @name @type
+(title2
+  (#set! "kind" "Interface")
+  ) @name @type
+(title3
+  (#set! "kind" "Interface")
+  ) @name @type
+(title4
+  (#set! "kind" "Interface")
+  ) @name @type
--- queries/tag.scm
+++
@@ -1,5 +0,0 @@
-(title1) @document.title
-(title2) @document.title
-(title3) @document.title
-(title4) @document.title
-(title5) @document.title
