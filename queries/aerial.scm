cc5f05aae88cfe5d1854ea26606c023b88c7e130
DragonBillow <DragonBillow@outlook.com>
2023-12-22 13:39:27 +0800

    feat: copy code from markdown

--- queries/aerial.scm
+++ queries/aerial.scm
@@ -1,5 +1,11 @@
-(title1
-  (atx_h1_marker) @level
-  (title_content) @name
+(atx_heading
+  [(atx_h1_marker) (atx_h2_marker) (atx_h3_marker) (atx_h4_marker) (atx_h5_marker) (atx_h6_marker)] @level
+  heading_content: (_) @name
   (#set! "kind" "Interface")
-) @type
+  ) @symbol
+
+(setext_heading
+  heading_content: (_) @name
+  (#set! "kind" "Interface")
+  [(setext_h1_underline) (setext_h2_underline)] @level
+  ) @symbol
