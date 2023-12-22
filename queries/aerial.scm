577c32d4f48807bd9b812ff479a1032a89e65377
DragonBillow <DragonBillow@outlook.com>
2023-12-22 13:39:05 +0800

    feat(aerial): add aerial.scm

---
+++ queries/aerial.scm
@@ -0,0 +1,25 @@
+(title0
+  (title_marker) @level
+  (title_content) @name
+  (#set! "kind" "Interface")
+) @type
+(title1
+  (title_marker) @level
+  (title_content) @name
+  (#set! "kind" "Interface")
+) @type
+(title2
+  (title_marker) @level
+  (title_content) @name
+  (#set! "kind" "Interface")
+) @type
+(title3
+  (title_marker) @level
+  (title_content) @name
+  (#set! "kind" "Interface")
+) @type
+(title4
+  (title_marker) @level
+  (title_content) @name
+  (#set! "kind" "Interface")
+) @type
